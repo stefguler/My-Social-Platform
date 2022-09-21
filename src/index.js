440cd86319e438c0786d41c9f6f2b5e49d650f73
Stef <stef.guler@gmail.com>
2022-09-21 10:40:01 +0200

    small change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import  store from './redux/Store'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { FindFriends } from './Components/FindFriends/FindFriends'
+import { FindFriends } from './components/FindFriends/FindFriends'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/findfriends" element={<FindFriends />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   </Provider>
