5b291f5e254f63e568b91aabb85b4a2a93786582
Miri Yoo <violinmiriu@gmail.com>
2022-09-22 18:25:32 +0200

    check login credentials

--- src/Components/LoginSection/PagesRight/01_SignIn/Page1.js
+++ src/Components/LoginSection/PagesRight/01_SignIn/Page1.js
@@ -1,14 +1,19 @@
 import { useState } from "react"
-import { ButtonQuestion, ButtonsContainer, ButtonSignUp, FormContainer, FormEmail, FormPassword, FormStyledIcon, FormSubmit, FormTitle, FormUserName, Frame4, PageDots, RightContainer, Space, StyledInput, FormSubmitButton, ButtonSignUpButton } from "./Page1.styles"
+import { ButtonQuestion, ButtonsContainer, ButtonSignUp, FormContainer, FormEmail, FormPassword, FormStyledIcon, FormSubmit, FormTitle, FormUserName, Frame4, PageDots, RightContainer, Space, StyledInput, FormSubmitButton, ButtonSignUpButton, ErrorContainer } from "./Page1.styles"
 import { FaUserCircle, FaLock } from "react-icons/fa"
 import { useNavigate } from 'react-router-dom'
 import TextField from '@mui/material/TextField'
+import { setAccessToken, setRefreshToken, setIsInputValid, selectIsInputValid, setCurrentUser } from '../../../../redux/slices/Auth'
+import { useDispatch, useSelector } from "react-redux"
+
 
 function Page1() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const navigate = useNavigate();
+    const isInputValid = useSelector(selectIsInputValid)
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value)
@@ -20,11 +25,10 @@ function Page1() {
         e.preventDefault()
         console.log("Email: ", email)
         console.log("Password: ", password)
-        navigate('/posts')
 
 
         const url = "https://motion.propulsion-home.ch/backend/api/auth/token/"
-        const jsObject = {
+        const credentials = {
             email: email,
             password: password
         }
@@ -33,9 +37,26 @@ function Page1() {
             headers: new Headers({
                 "Content-Type": "application/json"
             }),
-            body: JSON.stringify(jsObject)
+            body: JSON.stringify(credentials)
         }
         fetch(url, config)
+            .then(result => {
+                if (!result.ok) {
+                    dispatch(setIsInputValid(false))
+                } else {
+                    result.json().then(data => {
+                        const accessToken = data.access
+                        const refreshToken = data.refresh
+                        const username = data.user.username
+                        dispatch(setAccessToken(accessToken))
+                        dispatch(setRefreshToken(refreshToken))
+                        dispatch(setCurrentUser(username))
+                        navigate('/posts')
+                    })
+                }
+            }
+        )
+
     }
 
     return (
@@ -66,7 +87,7 @@ function Page1() {
                             <FaUserCircle size={20} />
                         </FormStyledIcon>
                         <Space />
-                        <TextField type="text" onChange={handleEmailChange} name={"username"} id="8" label="Username" variant="standard" fullWidth/>
+                        <TextField type="text" onChange={handleEmailChange} name={"username"} id="8" label="Username" variant="standard" fullWidth />
                     </FormUserName>
 
                     <FormPassword>
@@ -74,9 +95,12 @@ function Page1() {
                             <FaLock size={20} />
                         </FormStyledIcon>
                         <Space />
-                        <TextField type="password" onChange={handlePasswordChange} name={"password"} id="9" label="Password" variant="standard" fullWidth/>
+                        <TextField type="password" onChange={handlePasswordChange} name={"password"} id="9" label="Password" variant="standard" fullWidth />
                     </FormPassword>
-
+                    {isInputValid ? null : <ErrorContainer>
+                        <p>username or password invalid</p>
+                    </ErrorContainer>
+                    }
                     <FormEmail />
                     <Frame4>
                         <FormSubmit>
@@ -96,5 +120,5 @@ function Page1() {
     )
 
 }
-
+// export default connect(mapStateToProps)(MyComponent);
 export default Page1
