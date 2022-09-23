f03ae01e53f95b3e788e2bff462fe1635abb3467
Stef <stef.guler@gmail.com>
2022-09-23 09:01:32 +0200

    updates

--- src/Components/ProfileSection/ProfileSection.js
+++ src/Components/ProfileSection/ProfileSection.js
@@ -1,11 +1,14 @@
 import ProfileInfo from "./ProfileHeader/ProfileInfo";
 import UserPosts from "./UserPosts/UserPosts";
+import Header from "../HeaderSection/Header";
 
 
 export default function ProfileSection() {
 
+
   return (
     <>
+        <Header/>
         <ProfileInfo />
         <UserPosts />
     </>
